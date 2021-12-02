48d246c10eaf893aef0c4e807ce66862711055ac
AleksTereh <aleks.tereh@yandex.ru>
2021-12-02 23:22:41 +0300

    кнопка с темами

--- app/src/main/java/ru/alekstereh/myapplication/SettingsActivity.java
+++ app/src/main/java/ru/alekstereh/myapplication/SettingsActivity.java
@@ -1,8 +1,11 @@
 package ru.alekstereh.myapplication;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ActionMenuView;
+import android.widget.Button;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.radiobutton.MaterialRadioButton;
 
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String NameSharedPreference = "LOGIN";
     public static final String AppTheme = "APP_THEME";
-    public static final int MyCoolCodeStyle = 0;
-    public static final int AppThemeLightCodeStyle = 1;
-    public static final int AppThemeRedCodeStyle = 2;
+    protected static final int MyCoolCodeStyle = 0;
+    protected static final int AppThemeLightCodeStyle = 1;
+    protected static final int AppThemeRedCodeStyle = 2;
 
 
     @Override
@@ -26,8 +29,22 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
 
         initThemeChooser();
+
+        Button btnTheme = findViewById(R.id.btnTheme);
+        btnTheme.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId() == R.id.btnTheme) {
+            Intent intent = new Intent(this, ActionMenuView.class);
+            break;
+            default:
+                break;
+        }
+    }
 
 
     private void initThemeChooser() {
@@ -43,19 +60,15 @@ public class SettingsActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // сохраним настройки
                 setAppTheme(codeStyle);
-                // пересоздадим активити, чтобы тема применилась
                 recreate();
             }
         });
     }
 
 
-    // Сохранение настроек
     protected void setAppTheme(int codeStyle) {
         SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
-        // Настройки сохраняются посредством специального класса editor.
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt(AppTheme, codeStyle);
         editor.apply();
@@ -63,13 +76,12 @@ public class SettingsActivity extends AppCompatActivity {
 
 
     private int getAppTheme(int codeStyle) {
+
         return codeStyleToStyleId(getCodeStyle(codeStyle));
     }
 
     protected int getCodeStyle(int codeStyle) {
-        // Работаем через специальный класс сохранения и чтения настроек
         SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
-        //Прочитать тему, если настройка не найдена - взять по умолчанию
         return sharedPref.getInt(AppTheme, codeStyle);
     }
 
